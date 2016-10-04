9f50de88b5150a6ab19c2f2c86343c546bf269d8
AustinZzx <zexiazha@usc.edu>
2016-10-03 18:01:49 -0700

    Update publish.py
    now we can publish several robots with various xyz vectors

--- publish.py
+++ publish.py
@@ -1,13 +1,23 @@
 import paho.mqtt.client as paho
 from randomcoordinate import initial
 from randomcoordinate import movement
+from signal import signal, SIGPIPE, SIG_DFL
 
-x,y,z = initial()
-a = 0
-c = paho.Client()
-c.connect("127.0.0.1", 1883)
+def rbtpublish():
+   signal(SIGPIPE, SIG_DFL)
+   b = 0
+   c = paho.Client()
+   c.connect("127.0.0.1", 1883)
 
-while a < 500:
-   x,y,z = movement(x,y,z)
-   c.publish("test",x)
-   a = a+1
+   num = int(raw_input("Enter the number of robots you want to see: "))
+
+   while b < num:
+      x,y,z = initial()
+      a = 0
+      while a < 500:
+         x,y,z = movement(x,y,z)
+         c.publish("robot%i" %b,str(x)+" "+str(y)+" "+str(z))
+         a = a+1
+      b = b+1
+      
+rbtpublish()
