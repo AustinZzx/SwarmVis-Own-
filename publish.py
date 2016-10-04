651929a462374bd2767c23beef81eb1e62ef0d3c
AustinZzx <zexiazha@usc.edu>
2016-10-03 22:48:05 -0700

    Update publish.py
    modify it to match with the subscribe program

--- publish.py
+++ publish.py
@@ -2,22 +2,29 @@ import paho.mqtt.client as paho
 from randomcoordinate import initial
 from randomcoordinate import movement
 from signal import signal, SIGPIPE, SIG_DFL
+import time
 
-def rbtpublish():
+def on_publish(client, userdata, mid):
+    print("mid: "+str(mid))
+    
+def rbtpublish(num):
    signal(SIGPIPE, SIG_DFL)
    b = 0
    c = paho.Client()
+   c.on_publish = on_publish
    c.connect("127.0.0.1", 1883)
-
-   num = int(raw_input("Enter the number of robots you want to see: "))
-
+   c.loop_start()  
    while b < num:
       x,y,z = initial()
       a = 0
-      while a < 500:
+      while a < 30:
          x,y,z = movement(x,y,z)
-         c.publish("robot%i" %b,str(x)+" "+str(y)+" "+str(z))
+         (rc, mid) =c.publish("robot%i" %(b+1),str(x)+" "+str(y)+" "+ \
+                    str(z))
          a = a+1
       b = b+1
+   time.sleep(1)
       
-rbtpublish()
+      
+num = int(raw_input("Enter the number of robots you want to see: "))
+rbtpublish(num)
